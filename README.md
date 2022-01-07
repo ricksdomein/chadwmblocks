e00edcbcfb9ec25aba74ad5b4485a9eb24b49ab7
Rick <rick@ricksdomein.nl>
2022-01-07 08:49:01 +0100

    Removed Luke's stuff

--- README.md
+++ README.md
@@ -7,14 +7,6 @@ Modular status bar for dwm written in c.
 The statusbar is made from text output from commandline programs.  Blocks are
 added and removed by editing the config.h file.
 
-# Luke's build
-
-I have dwmblocks read my preexisting scripts
-[here in my dotfiles repo](https://github.com/LukeSmithxyz/voidrice/tree/master/.local/bin/statusbar).
-So if you want my build out of the box, download those and put them in your
-`$PATH`. I do this to avoid redundancy in LARBS, both i3 and dwm use the same
-statusbar scripts.
-
 # Signaling changes
 
 Most statusbars constantly rerun every script every several seconds to update.
@@ -38,7 +30,3 @@ Note that all modules must have different signal numbers.
 Like i3blocks, this build allows you to build in additional actions into your
 scripts in response to click events.  See the above linked scripts for examples
 of this using the `$BLOCK_BUTTON` variable.
-
-For this feature to work, you need the appropriate patch in dwm as well. See
-[here](https://dwm.suckless.org/patches/statuscmd/).
-Credit for those patches goes to Daniel Bylinka (daniel.bylinka@gmail.com).
--- config.h
+++ config.h
@@ -1,36 +1,26 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
-	{"",	"sb-music",	0,	11},
-	{"",	"sb-pacpackages",	0,	8},
-	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
-	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
-	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
-	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
-	{"",	"sb-nettraf",	1,	16},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	{"",	"sb-torrent",		30,	9},
+        {"",	"sb-nettraf",		1,	10},
+        {"",	"sb-disk",		30,	11},
+        {"",	"sb-disk $HOME",	30,	12},
+        {"",	"sb-rss",         	0,      8},
+        {"",	"sb-updates",		3600,   7},
+        {"",	"sb-backlight",   	0,      6},
+        {"",	"sb-battery",    	1,      5},
+        {"",	"sb-cpu",        	1,      4},
+        {"",	"sb-mem",        	1,      3},
+        {"",	"sb-mic",      		0,      13},
+        {"",	"sb-vol",      		0,      2},
+        {"",	"sb-clock",       	0,      1},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "  ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
