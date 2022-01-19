37bbd80bcad8ccaed66c3aed6b5742c3ec7dd4c8
Rick <rick@ricksdomein.nl>
2022-01-19 16:57:16 +0100

    mpd module

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-music",		0,	15},
 	{"",	"sb-torrent",		30,	9},
         {"",	"sb-nettraf",		1,	10},
         {"",	"sb-disk",		30,	11},
